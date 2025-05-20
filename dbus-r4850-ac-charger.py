2223f3d33a165c5b74455009e6fa67f2d3bd7ad4
TheBarnHome <126877271+TheBarnHome@users.noreply.github.com>
2025-05-20 09:26:48 +0000

    clean imports

--- dbus-r4850-ac-charger.py
+++ dbus-r4850-ac-charger.py
@@ -13,16 +13,8 @@ import argparse
 import logging
 import sys
 import os
-import json
-from enum import Enum
 import datetime
 import dbus
-import dbus.service
-import subprocess
-import time
-import atexit
-import concurrent.futures
-from inverterd import Client, Format
 
 logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
